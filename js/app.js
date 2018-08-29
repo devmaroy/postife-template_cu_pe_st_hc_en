46e8d26acc7cebe026ac522da6d21343d12ddd27
Marek Matejovic <me@devmaroy.cz>
2018-08-29 15:22:24 +0200

    Homepage almost done

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 		$('.hamburger-icon').toggleClass('is-open');
 		$('.site-navigation').toggleClass('is-open');
 		$('.site-header').toggleClass('is-open');
+		$('.site-header-content').toggleClass('is-open');
 	});
 
 })(jQuery)
