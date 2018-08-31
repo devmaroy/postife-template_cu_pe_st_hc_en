274f0b82340c83c3443f83bca159764582caace3
Marek Matejovic <me@devmaroy.cz>
2018-08-31 11:31:17 +0200

    FAQ done

--- js/app.js
+++ js/app.js
@@ -7,4 +7,28 @@
 		$('.site-header-content').toggleClass('is-open');
 	});
 
+
+	// FAQ Accordion
+	var faqList = $( '.faq-list' ),
+	    faqListDt = faqList.find('dt');
+
+	// Hide FAQ Answer
+	faqList.find( 'dd' ).slideUp();
+
+	// FAQ List
+	faqListDt.on('click', function() {
+		var dt = $( this ),
+			dd = dt.next( 'dd' );
+
+		// Find panel icon for this dt
+		var panelIcon = dt.find( '.panel-icon' );
+
+		// Expand or collapse panel
+		dd.slideToggle();
+
+		// Toggle '+' and '-'
+		panelIcon.toggleClass( 'fa-plus' )
+			.toggleClass( 'fa-minus' );
+	});
+
 })(jQuery)
